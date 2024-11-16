a29772272235896aac8d42213679b5a8be484b00
Fahad <fahad.45854@iqra.edu.com>
2024-11-16 23:06:10 +0500

    dynamic page created

---
+++ src/pages/id/[id].tsx
@@ -0,0 +1,14 @@
+import { CardWithForm } from '@/components/Card'
+import React from 'react'
+
+const Id = () => {
+    return (
+        <div
+            className="flex justify-center items-center h-screen"
+        >
+            <CardWithForm />
+        </div>
+    )
+}
+
+export default Id
